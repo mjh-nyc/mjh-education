10c0513ac3ef3c2f249f977f4aff1ee26c621ad0
Al Sierra <al@cloudred.com>
2018-11-01 17:42:40 -0400

    Remove var dump

--- web/app/themes/mjh-edu/app/Controllers/App.php
+++ web/app/themes/mjh-edu/app/Controllers/App.php
@@ -138,7 +138,6 @@ class App extends Controller
 			$cats = array();
 			foreach ($post_categories as $cat_id) {
 				$cat = get_category($cat_id);
-				var_dump($cat);
 				$cats[] = array('name' => $cat->name, 'link' => get_term_link($cat->term_id));
 			}
 			return $cats;
