5535d72ab79b8177af630d6fc9f7a833f5c734f2
Al Sierra <al@cloudred.com>
2018-11-02 18:01:24 -0400

    Get survivors permalink to their tagged survivor_story post type

--- web/app/themes/mjh-edu/app/Controllers/TemplateSurvivorsListing.php
+++ web/app/themes/mjh-edu/app/Controllers/TemplateSurvivorsListing.php
@@ -27,19 +27,47 @@ class TemplateSurvivorsListing extends Controller
 			'hide_empty' => false,
 		) );
 		if (!empty($survivors)) {
-			$cats = array();
-			foreach ($survivors as $cat_id) {
-				$cat = get_category($cat_id);
-				$cats[] = array(
-					'name' => $cat->name,
-					'link' => get_term_link($cat->term_id),
-					'survivors_current_image' => get_field('survivors_current_image','survivors_'.$cat->term_id),
-					'survivors_past_image' => get_field('survivors_past_image','survivors_'.$cat->term_id),
+			$terms = array();
+			foreach ($survivors as $term_id) {
+				$term = get_category($term_id);
+				$terms[] = array(
+					'name' => $term->name,
+					'link' => $this->getInitSurvivorStoryPostLink($term_id),
+					'survivors_current_image' => get_field('survivors_current_image','survivors_'.$term->term_id),
+					'survivors_past_image' => get_field('survivors_past_image','survivors_'.$term->term_id),
 				);
 			}
-			return $cats;
+			return $terms;
 		}else{
 			return array();
+		}
+	}
+	/**
+	 * Get survivors permalink to their tagged survivor_story post type. Default to term link if no post found
+	 *
+	 * @return string
+	 */
+	private function getInitSurvivorStoryPostLink($term_id){
+		$args = array(
+			'post_type' => 'survivor_story',
+			'posts_per_page' => 1,
+			'post_status' => 'publish',
+			'tax_query' => array(
+				array(
+					'taxonomy' => 'survivors',
+					'field'    => 'term_id',
+					'terms'    => $term_id,
+				),
+			),
+			'orderby' => 'menu_order',
+			'order' => 'asc',
+		);
+		$survivor_stories = new WP_Query( $args);
+		if($survivor_stories->post_count){
+			$post = $survivor_stories->next_post();
+			return get_permalink($post->ID);
+		}else{
+			return get_term_link($term_id);
 		}
 	}
 }
