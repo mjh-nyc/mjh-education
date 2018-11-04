9bbb5f9cd531a8f3d37c1567e4ca24189aaddb98
Al Sierra <al@cloudred.com>
2018-11-03 23:13:49 -0400

    Make sign and download consistent Use the content no matter what login state

--- web/app/themes/mjh-edu/resources/views/partials/content-single-lessons.blade.php
+++ web/app/themes/mjh-edu/resources/views/partials/content-single-lessons.blade.php
@@ -3,13 +3,11 @@
         @include('partials.content-share')
     </header>
     <div class="entry-content">
-        @if(is_user_logged_in())
-            @php the_content() @endphp
-        @else
-            <h2>{{ __('About this lesson', 'sage') }}</h2>
-            @php the_excerpt() @endphp
+        <h2>{{ __('About this lesson', 'sage') }}</h2>
+        @php the_content() @endphp
+        @if(App::get_field('download'))
+            @include('partials.content-download-lesson-link')
         @endif
-        @include('partials.content-download-lesson-link')
     </div>
     <footer>
         {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
