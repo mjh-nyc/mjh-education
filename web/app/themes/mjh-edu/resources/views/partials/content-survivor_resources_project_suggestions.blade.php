db4912a5640832703a371085e052d2ff461a3139
Al Sierra <al@cloudred.com>
2018-11-07 01:10:49 -0500

    Set up survivor resource project suggestions

---
+++ web/app/themes/mjh-edu/resources/views/partials/content-survivor_resources_project_suggestions.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <h1 class="entry-title">{!! __('Project Suggestions For '.$survivor.'’s Story','sage') !!}</h1>
+</header>
+<div class="entry-content">
+    <div class="content">{!! get_field('project_suggestions_list')  !!}</div>
+</div>
