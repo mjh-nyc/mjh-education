e2b93768dbb12a4ec69f2921570ede95fa746bbe
Cyril Tsiboulski <ct@cloudred.com>
2018-11-11 11:19:43 -0500

    Added secondary footer

--- web/app/themes/mjh-edu/resources/views/partials/footer.blade.php
+++ web/app/themes/mjh-edu/resources/views/partials/footer.blade.php
@@ -4,4 +4,16 @@
     	@php dynamic_sidebar('sidebar-footer') @endphp
     </div>
   </div>
+  <div class="container secondary">
+  	<div class="row">
+  		<div class="col-md-8">
+    		&#169; {{ date("Y") }} {!! _e('Museum of Jewish Heritage—A Living Memorial to the Holocaust. All rights reserved.','sage') !!}
+    	</div>
+    	<div class="col-md-4">
+    		@if (has_nav_menu('footer_navigation'))
+            	{!! wp_nav_menu(['theme_location' => 'footer_navigation']) !!}
+          	@endif
+    	</div>
+    </div>
+  </div>
 </footer>
