932cd94a49951d61c69d60fc25865543d8c9bab8
Al Sierra <al@cloudred.com>
2018-11-27 12:34:33 -0500

    Add redirect option to custom login form

--- web/app/themes/mjh-edu/resources/views/partials/content-download-lesson-link.blade.php
+++ web/app/themes/mjh-edu/resources/views/partials/content-download-lesson-link.blade.php
@@ -3,5 +3,5 @@
         <a class="cta cta-white cta-icon cta-download animsition-link" href="{{App::get_field('download')}}">{{ __('Download This Lesson Plan', 'sage') }}</a>
     @endif
 @else
-    <a class="cta cta-black cta-icon cta-arrow animsition-link" href="{{wp_login_url( get_permalink() )}}">{{ __('Sign in to see more', 'sage') }}</a>
+    <a class="cta cta-black cta-icon cta-arrow animsition-link" href="{!! get_home_url( ) !!}/login?redirect_to={!! urlencode(get_permalink()) !!}">{{ __('Sign in to see more', 'sage') }}</a>
 @endif
