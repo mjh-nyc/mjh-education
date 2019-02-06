c8cbebcb58c4b5578842b035c43a94eeb9d71f45
Cyril Tsiboulski <ct@cloudred.com>
2019-02-06 11:45:04 -0500

    Updated lessons listing page with grid

--- web/app/themes/mjh-edu/resources/views/template-lessons-listing.blade.php
+++ web/app/themes/mjh-edu/resources/views/template-lessons-listing.blade.php
@@ -2,10 +2,15 @@
   Template Name: Lessons Listing Template
 --}}
 
-@extends('layouts.app')
+@extends('layouts.testimonies-and-artifacts')
 
 @section('content')
-    @while(have_posts()) @php the_post() @endphp
-        @include('partials.lessons-listing')
-    @endwhile
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.page-header')
+    @include('partials.content-page')
+  @endwhile
+@endsection
+
+@section('testimonies-and-artifacts')
+  @include('partials.lessons-listing')
 @endsection
