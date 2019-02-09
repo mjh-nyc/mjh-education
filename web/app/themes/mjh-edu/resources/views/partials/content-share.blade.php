ca6fdaf0ee85ffd37abdcddcbe66df791c43d76b
Al Sierra <al@cloudred.com>
2019-02-09 00:13:18 -0500

    Powerscan fixes

--- web/app/themes/mjh-edu/resources/views/partials/content-share.blade.php
+++ web/app/themes/mjh-edu/resources/views/partials/content-share.blade.php
@@ -11,7 +11,7 @@
                 <a href="https://twitter.com/intent/tweet?text={!! urlencode(App::postTitle()); !!}&url={!! get_the_permalink(); !!}&via={!! App::get_field('twitter_handle', 'option') !!}" onclick="ga('send', 'event', 'social', 'share', 'twitter');" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
             </div>
             <div class="share-channel">
-                <a href="mailto:?&subject=I thought you might find this interesting!&body={!! App::postTitle(); !!}%0A%0A{!! get_the_permalink(); !!}" onclick="ga('send', 'event', 'social', 'share', 'email');"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+                <a href="mailto:?subject=I thought you might find this interesting!&body={!! rawurlencode(App::postTitle()); !!}%0A%0A{!! get_the_permalink(); !!}" onclick="ga('send', 'event', 'social', 'share', 'email');"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
             </div>
 
 
